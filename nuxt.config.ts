7eee239f74c56491c85f24dad587e5776fe3adcc
everleandro <ever.leandros91@gmail.com>
2023-12-30 22:07:27 -0600

    add google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,17 @@ export default defineNuxtConfig({
           content: "default description",
         },
       ],
+      script: [
+        {
+          src: "https://www.googletagmanager.com/gtag/js?id=G-RGYB1GJW1C",
+          type: "text/javascript",
+          async: true,
+        },
+        {
+          children:
+            "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-RGYB1GJW1C');",
+        },
+      ],
     },
   },
   vite: {
